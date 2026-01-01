207c0ba22e0c8b4d942f6aead7929da2b1abe5e5
FinalStaticFox <16882447+FinalStaticFox@users.noreply.github.com>
2026-01-01 12:22:40 +0100

    [DO NOT MERGE] Added failing test on purpose

--- tests/mis.test.ts
+++ tests/mis.test.ts
@@ -4,3 +4,26 @@ import { calculateSignalStrength } from "../src/MinecraftItemBuilder";
 test("Empty chest has no redstone signal", () => {
   expect(calculateSignalStrength([])).toBe(0);
 });
+
+// Bad test on purpose DO NOT MERGE
+test("Chest with one 64-stack item has no redstone signal [DO NOT MERGE]", () => {
+  expect(
+    calculateSignalStrength([
+      {
+        id: "",
+        name: "",
+        sprite: "",
+        color: {
+          rgb: [0, 0, 0],
+          hsl: [0, 0, 0],
+          hex: "",
+        },
+        category: [],
+        group: [],
+        material: "",
+        stack_size: 64,
+        rarity: "",
+      },
+    ])
+  ).toBe(0);
+});
